7fa7904ec008a3558d511a2a0cccd31dfc042c37
Felix Cena Jr. <felixcena15@gmail.com>
2023-11-14 10:16:51 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LandingMainComponent } from './landing-main/landing-main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LandingMainComponent
   ],
   imports: [
     BrowserModule,
