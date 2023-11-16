6d39b40fd55d08c700e20e4f86e4c9d6630e78a8
cor <m.masong14@gmail.com>
2023-11-15 19:51:21 -0800

    added route to borrower registration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { LibraryComponent } from './library/library.component';
 import { ContactComponent } from './contact/contact.component';
 import { LandingMainComponent } from './landing-main/landing-main.component';
+import { BorrowerRegistrationComponent } from './borrower-registration/borrower-registration.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'library', component: LibraryComponent },
   { path: 'contact', component: ContactComponent },
   { path: '', component: LandingMainComponent }, 
+  { path: 'borrower-registration', component: BorrowerRegistrationComponent},
   { path: '**', redirectTo: '/home' }
 
 ];
