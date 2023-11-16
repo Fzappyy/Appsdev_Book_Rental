1dd49a223cf373007c51765588d7d2a53ca93c16
cor <m.masong14@gmail.com>
2023-11-15 19:13:19 -0800

    landing component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LandingMainComponent } from './landing-main/landing-main.component';
+import { LibraryComponent } from './library/library.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LandingMainComponent
+    LandingMainComponent,
+    LibraryComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
